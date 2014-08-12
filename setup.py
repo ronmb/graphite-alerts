78150df48976bf9705a23e8aebbfefbb66d14e49
ronmb <ronmb@icloud.com>
2014-08-11 17:04:00 -0700

    Debug 3

--- setup.py
+++ setup.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python
 import os
-from setuptools import setup
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from distutils.core import setup
 
 
-def run_setup():
-    setup(
-        name='graphitealerts',
-        version='0.0.1',
-        description='',
-        keywords = '',
-        url='http://github.com/ronmb/graphite-alerts',
-        author='Aybars Badur',
-        author_email='aybars.badur@gmail.com',
-        license='BSD',
-        # packages=['graphitealerts'],
-        install_requires=[
-            'Jinja2==2.6',
-            'PyYAML==3.10',
-            'distribute==0.6.28',
-            'pagerduty==0.2.1',
-            'redis==2.6.2',
-            'requests==0.14.0',
-            'python-simple-hipchat==0.1',
-        ],
-        test_suite='tests',
-        # long_description=read('README.md'),
-        entry_points="""
-        [console_scripts]
-        graphite-alerts=graphitealerts.worker:run
-        alerts-dev-server=graphitealerts.app:run
-        """,
-    )
+setup(
+    name='graphitealerts',
+    version='0.0.1',
+    description='',
+    keywords = '',
+    url='http://github.com/ronmb/graphite-alerts',
+    author='Aybars Badur',
+    author_email='aybars.badur@gmail.com',
+    license='BSD',
+    # packages=['graphitealerts'],
+    install_requires=[
+        'Jinja2==2.6',
+        'PyYAML==3.10',
+        'distribute==0.6.28',
+        'pagerduty==0.2.1',
+        'redis==2.6.2',
+        'requests==0.14.0',
+        'python-simple-hipchat==0.1',
+    ],
+    test_suite='tests',
+    # long_description=read('README.md'),
+    entry_points="""
+    [console_scripts]
+    graphite-alerts=graphitealerts.worker:run
+    alerts-dev-server=graphitealerts.app:run
+    """,
+)
 
 if __name__ == '__main__':
-    run_setup()
+    setuptools.run_setup()
